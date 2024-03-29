30dcbcae0b60a8c8e11b6d1358cefd0bb23b608e
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-29 18:07:18 +0200

    create menu items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import styles from "./style";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Menu from './components/Menu'
+
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
@@ -13,6 +15,7 @@ const App = () => {
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
+          <Menu />
           <Footer />
         </div>
       </div>
---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import { useState } from  'react'
+import menu from '../data/menu'
+import MenuItem from './MenuItem'
+
+const Menu = ({maxItemPerPage=3}) => {
+    const [displayedItems, setDisplayedItems] = useState(maxItemPerPage);
+
+    const handleViewMore =()=>{
+        setDisplayedItems(prevDisplayedItems => prevDisplayedItems + maxItemPerPage);
+    }
+
+    return (
+        <div id="projects" className="flex flex-col md:flex-row items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {menu.slice(0, displayedItems).map((item) => (
+                    <MenuItem
+                        title={item.title}
+                        description={item.description}
+                        price={item.price}
+                        imgUrl={item.imgUrl}
+                    />
+                ))}
+            </div>
+            {displayedItems < menu.length && (
+                <button onClick={handleViewMore} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    View More
+                </button>
+            )}
+        </div>
+
+    )
+}
+
+export default Menu
