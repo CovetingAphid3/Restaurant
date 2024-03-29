1e90c79646ed7a85eb2cd0b9d2619553f7d374c3
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-29 16:19:57 +0200

    init

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,15 @@
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import logo from "./logo.svg";
+import close from "./close.svg";
+import menu from "./menu.svg";
+
+export {
+    facebook,
+    instagram,
+    twitter,
+    logo,
+    close,
+    menu
+}
