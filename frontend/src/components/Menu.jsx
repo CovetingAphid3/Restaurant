7bc9dc09d121e8eda37b046a79302da07702942b
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-30 10:57:00 +0200

    update button

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from  'react'
 import menu from '../data/menu'
 import MenuItem from './MenuItem'
+import { Button } from '@mui/material';
 
 const Menu = ({maxItemPerPage=3}) => {
     const [displayedItems, setDisplayedItems] = useState(maxItemPerPage);
@@ -10,7 +11,7 @@ const Menu = ({maxItemPerPage=3}) => {
     }
 
     return (
-        <div id="projects" className="flex flex-col md:flex-row items-center justify-center">
+        <div id="projects" className="flex flex-row md:flex-row items-center justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {menu.slice(0, displayedItems).map((item) => (
                     <MenuItem
@@ -21,11 +22,13 @@ const Menu = ({maxItemPerPage=3}) => {
                     />
                 ))}
             </div>
-            {displayedItems < menu.length && (
-                <button onClick={handleViewMore} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                    View More
-                </button>
-            )}
+            <div>
+                {displayedItems < menu.length && (
+                    <Button variant="outlined" onClick={handleViewMore} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                        View More
+                    </Button>
+                )}
+            </div>
         </div>
 
     )
